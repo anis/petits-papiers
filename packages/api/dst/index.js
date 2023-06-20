64260f5bf172337d3e5a68bd2a5524aa3f7d6b8c
Anis Safine Laget <anis@users.noreply.github.com>
2023-06-20 17:16:27 +0200

    feat: init an empty express server

---
+++ packages/api/dst/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+app.listen(3001, function () { return console.log("Server is running"); });
---
+++ packages/api/src/index.ts
@@ -0,0 +1,4 @@
+import * as express from "express";
+
+const app = express();
+app.listen(3001, () => console.log("Server is running"));
